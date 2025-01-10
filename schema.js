c55310c696968cf01eb4a7ba3194b1221b5394d6
Suresh Bishnoi <kanwasures123@gmail.com>
2025-01-10 10:38:36 +0530

    i think i am getting error in this

--- schema.js
+++ schema.js
@@ -1,15 +1,15 @@
-const Joi=require("joi");
+const Joi = require("joi");
 
-module.exports.listingSchema=Joi.object({
-    listing:Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        price:Joi.number().required(),
-        location:Joi.string().required(),
-        country:Joi.string().required().min(0),
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(), // Adjust length validation if needed
         image: Joi.object({
-            filename: Joi.string(),
+            filename: Joi.string().optional(),
             url: Joi.string().uri().required(),
         }).allow(null),
-    }).required()
-})
+    }).required(),
+});
